1b03f8fcefed19dc5868eadf24d731dd4ddffbed
Your Name <htcnhung148.96@gmail.com>
2021-04-26 22:11:12 +0700

    up

---
+++ module1/07_bien_kieu_du_lieu_toan_tu/thuc_hanh/su_dung_toan_tu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+inputWidth = prompt("Enter the width");
+let width = parseInt(inputWidth);
+let inputHeight;
+inputHeight = prompt("Enter the height");
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích hình chữ nhật là: " + area);
