b68feee6eb9e4f8dcc33a8225fe1d64cfc345a90
Your Name <htcnhung148.96@gmail.com>
2021-07-25 17:33:22 +0700

    up bai

--- module2/src/_7_abstract_class_and_interface/bai_tap/bai_2/ColorableTest.java
+++ module2/src/_7_abstract_class_and_interface/bai_tap/bai_2/ColorableTest.java
@@ -6,10 +6,12 @@ public class ColorableTest {
         shapesArray[0] = new Circle(3);
         shapesArray[1] = new Rectangle(4, 6, "red", true);
         shapesArray[2] = new Square(5, "black", false);
+
         for (Shape shape : shapesArray) {
-            if (shape instanceof Colorable) {
+            if (shape instanceof Colorable) { // Vì sao nếu thay Colorable bằng Shape vẫn được???
                 System.out.println(shape.toString());
-                ((Colorable) shape).howToColor();
+
+                ((Colorable)shape).howToColor();
             } else {
                 System.out.println(shape.toString());
             }
