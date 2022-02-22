1b6211eeb629bf94a8929dc60dbd3cb16b2ffff1
htcnhung <htcnhung148.96@gmail.com>
2022-02-22 23:34:29 +0700

    upbai

--- module5/07_service_and_router/thuc_hanh/thuc_hanh_1/ThucHanh1/src/app/app.module.ts
+++ module5/07_service_and_router/thuc_hanh/thuc_hanh_1/ThucHanh1/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimelinesComponent } from './timelines/timelines.component';
+import { TimelinesTestComponent } from './timelines-test/timelines-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelinesComponent,
+    TimelinesTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
